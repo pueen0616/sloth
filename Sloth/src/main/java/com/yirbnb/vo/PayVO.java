b9f92fe7db3257e1cacfdab4dddf2a09877af153
user <user@HIYA-022T6JE2S0>
2020-10-30 16:16:13 +0900

    김도은

---
+++ Sloth/src/main/java/com/yirbnb/vo/PayVO.java
@@ -0,0 +1,32 @@
+package com.yirbnb.vo;
+
+import java.sql.Date;
+
+public class PayVO {
+	private int roomNum;
+	private Date cardNow;
+	private int cardNum;
+	public PayVO() {
+		super();
+	}
+	public int getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public Date getCardNow() {
+		return cardNow;
+	}
+	public void setCardNow(Date cardNow) {
+		this.cardNow = cardNow;
+	}
+	public int getCardNum() {
+		return cardNum;
+	}
+	public void setCardNum(int cardNum) {
+		this.cardNum = cardNum;
+	}
+	
+	
+}
---
+++ Sloth/src/main/java/com/yirbnb/vo/boardVO.java
@@ -0,0 +1,31 @@
+package com.yirbnb.vo;
+
+public class boardVO {
+	private String title;
+	private String content;
+	private String id;
+	public boardVO() {
+		super();
+		
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
