d1a25eeddd18e2dd8ab6741bf4f9756d40c19923
wooo <wooo@wooo-PC>
2020-10-30 16:14:09 +0900

    vo 청일

---
+++ Sloth/src/main/java/com/yirbnb/vo/reviewVO.java
@@ -0,0 +1,36 @@
+package com.yirbnb.vo;
+
+public class reviewVO {
+private int reserNum;
+private String reviewArea;
+private String reviewStar;
+private String id;
+public reviewVO() {
+	// TODO Auto-generated constructor stub
+}
+public int getReserNum() {
+	return reserNum;
+}
+public void setReserNum(int reserNum) {
+	this.reserNum = reserNum;
+}
+public String getReviewArea() {
+	return reviewArea;
+}
+public void setReviewArea(String reviewArea) {
+	this.reviewArea = reviewArea;
+}
+public String getReviewStar() {
+	return reviewStar;
+}
+public void setReviewStar(String reviewStar) {
+	this.reviewStar = reviewStar;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+
+}
