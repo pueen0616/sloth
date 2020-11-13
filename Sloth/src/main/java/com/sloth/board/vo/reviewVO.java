b4ea2e1034ce5ea01fed69a5bf922510adaeef37
joyeongrae <jyrent@naver.com>
2020-11-13 11:53:36 +0900

    v Change-Id: Ic73cec574ad18254c3b67b1da7bef773464602ff

--- Sloth/src/main/java/com/sloth/board/vo/reviewVO.java
+++ Sloth/src/main/java/com/sloth/board/vo/reviewVO.java
@@ -1,37 +1,56 @@
 package com.sloth.board.vo;
 
+import java.sql.Date;
+
 public class reviewVO {
-private int reserNum;
-private String reviewArea;
-private int reviewStar;
+private int review_num;
+private String review_area;
+private String room_name;
+private int review_star;
 private String id;
-public reviewVO() {
-	// TODO Auto-generated constructor stub
-}
-public int getReserNum() {
-	return reserNum;
-}
-public void setReserNum(int reserNum) {
-	this.reserNum = reserNum;
-}
-public String getReviewArea() {
-	return reviewArea;
-}
-public void setReviewArea(String reviewArea) {
-	this.reviewArea = reviewArea;
-}
+private Date today;
+private int room_num;
 
-public int getReviewStar() {
-	return reviewStar;
-}
-public void setReviewStar(int reviewStar) {
-	this.reviewStar = reviewStar;
-}
-public String getId() {
-	return id;
-}
-public void setId(String id) {
-	this.id = id;
-}
-
+	public int getReview_num() {
+		return review_num;
+	}
+	public void setReview_num(int review_num) {
+		this.review_num = review_num;
+	}
+	public String getReview_area() {
+		return review_area;
+	}
+	public void setReview_area(String review_area) {
+		this.review_area = review_area;
+	}
+	public String getRoom_name() {
+		return room_name;
+	}
+	public void setRoom_name(String room_name) {
+		this.room_name = room_name;
+	}
+	public int getReview_star() {
+		return review_star;
+	}
+	public void setReview_star(int review_star) {
+		this.review_star = review_star;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getToday() {
+		return today;
+	}
+	public void setToday(Date today) {
+		this.today = today;
+	}
+	public int getRoom_num() {
+		return room_num;
+	}
+	public void setRoom_num(int room_num) {
+		this.room_num = room_num;
+	}
 }
